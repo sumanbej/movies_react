887b49b0053b3b85f73fcb91748782548a77a7c4
Suman Bej <sumanbej.glt@gmail.com>
2024-05-02 22:43:27 +0530

    added basic folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import { useState , useEffect} from 'react'
 import { fetchDataFromApi } from './utils/api'
+import { useSelector , useDispatch } from 'react-redux'
+import { getApiConfiguration , getGenres } from './store/homeSlice'
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
 
 
 function App() {
+
+  const dispatch = useDispatch();
+  const {url} = useSelector((state) => state.home)
 
   useEffect(() =>{
     apiTest()
@@ -10,7 +22,8 @@ function App() {
   const apiTest = () => {
     fetchDataFromApi('/movie/popular').
     then((res) =>{
-      console.log(res)
+      console.log(res);
+      dispatch(getApiConfiguration(res));
 
     })
    
@@ -20,6 +33,7 @@ function App() {
   return (
     <>
      <h1>This is a movie project</h1> 
+     <div>{url?.total_pages}</div>
     </>
   )
 }
