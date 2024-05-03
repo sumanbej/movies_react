5bf37953b63102360bcc4bc2a960bc963d9cb735
Suman Bej <sumanbej.glt@gmail.com>
2024-05-03 23:15:12 +0530

    modified heroBanner and added useFetch hook

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, {useState } from 'react'
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import "./style.scss"
 
+import useFetch from '../../../hooks/useFetch';
+
 function HeroBanner() {
+
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  const { url } = useSelector((state) => state.home);
+  const { data, loading } = useFetch("/movie/upcoming");
+
+  useEffect(() => {
+    const bg =
+        url.backdrop +
+        data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+    setBackground(bg);
+}, [data]);
+
+
+
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+        navigate(`/search/${query}`);
+    }
+};
+
   return (
-    <div>HeroBanner</div>
+    <div className="heroBannerContent">
+    <span className="title">Welcome.</span>
+    <span className="subTitle">
+        Millions of movies, TV shows and people to discover.
+        Explore now.
+    </span>
+    <div className="searchInput">
+        <input
+            type="text"
+            placeholder="Search for a movie or tv show...."
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyUp={searchQueryHandler}
+        />
+        <button>Search</button>
+    </div>
+</div>
   )
 }
 
