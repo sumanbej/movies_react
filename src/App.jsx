389c5eef4e60e78ef9182d5b1113a21219c57103
Suman Bej <sumanbej.glt@gmail.com>
2024-05-05 17:47:43 +0530

    added Header and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import PageNotFound from "./pages/404/PageNotFound";
 function App() {
     const dispatch = useDispatch();
     const { url } = useSelector((state) => state.home);
-    console.log(url);
+    // console.log(url);
 
     useEffect(() => {
         fetchApiConfig();
@@ -26,7 +26,7 @@ function App() {
 
     const fetchApiConfig = () => {
         fetchDataFromApi("/configuration").then((res) => {
-            console.log(res);
+            // console.log(res);
 
             const url = {
                 backdrop: res.images.secure_base_url + "original",
@@ -48,7 +48,7 @@ function App() {
         });
 
         const data = await Promise.all(promises);
-        console.log(data);
+        // console.log(data);
         data.map(({ genres }) => {
             return genres.map((item) => (allGenres[item.id] = item));
         });
