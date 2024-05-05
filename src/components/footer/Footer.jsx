592c4fa91ad668da40070f3c7e208d8805a8be13
Suman Bej <sumanbej.glt@gmail.com>
2024-05-05 22:34:44 +0530

    Modified some components

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,51 +1,62 @@
-import React from "react";
-import {
-    FaFacebookF,
-    FaInstagram,
-    FaTwitter,
-    FaLinkedin,
-} from "react-icons/fa";
+  import React from "react";
+  import {
+      FaGithub,
+      FaInstagram,
+      FaTwitter,
+      FaLinkedin,
+  } from "react-icons/fa";
 
-import ContentWrapper from "../contentWrapper/ContentWrapper";
+  import ContentWrapper from "../contentWrapper/ContentWrapper";
 
-import "./style.scss";
+  import "./style.scss";
 
-const Footer = () => {
-    return (
-        <footer className="footer">
-            <ContentWrapper>
-                <ul className="menuItems">
-                    <li className="menuItem">Terms Of Use</li>
-                    <li className="menuItem">Privacy-Policy</li>
-                    <li className="menuItem">About</li>
-                    <li className="menuItem">Blog</li>
-                    <li className="menuItem">FAQ</li>
-                </ul>
-                <div className="infoText">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
-                </div>
-                <div className="socialIcons">
-                    <span className="icon">
-                        <FaFacebookF />
-                    </span>
-                    <span className="icon">
-                        <FaInstagram />
-                    </span>
-                    <span className="icon">
-                        <FaTwitter />
-                    </span>
-                    <span className="icon">
-                        <FaLinkedin />
-                    </span>
-                </div>
-            </ContentWrapper>
-        </footer>
-    );
-};
+  const Footer = () => {
+      return (
+          <footer className="footer">
+              <ContentWrapper>
+                  <ul className="menuItems">
+                      <li className="menuItem">Terms Of Use</li>
+                      <li className="menuItem">Privacy-Policy</li>
+                      <li className="menuItem">About</li>
+                      <li className="menuItem">Blog</li>
+                      <li className="menuItem">FAQ</li>
+                  </ul>
+                  <div className="infoText">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                      do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      Duis aute irure dolor in reprehenderit in voluptate velit
+                      esse cillum dolore eu fugiat nulla pariatur.
+                  </div>
+                  <div className="socialIcons">
+                  
+                  <a href="https://github.com/sumanbej" target="_blank" className="anc">
+                  <span className="icon">
+                      <FaGithub />
+                      </span>
+                      </a>
+                    
+                      
+                      <a href="https://www.instagram.com/bej.suman_/" target="_blank">
+                      <span className="icon">
+                          <FaInstagram />
+                      </span>
+                      </a>
+                      <a href="https://twitter.com/bej_glt" target="_blank">
+                      <span className="icon">
+                          <FaTwitter />
+                      </span>
+                      </a>
+                      <a href="https://www.linkedin.com/in/suman-bej-3aa806180/" target="_blank">
+                      <span className="icon">
+                          <FaLinkedin />
+                      </span>
+                      </a>
+                  </div>
+              </ContentWrapper>
+          </footer>
+      );
+  };
 
-export default Footer;
+  export default Footer;
