11c1f30c99fef8da49452e5103446640d26558a9
Suman Bej <sumanbej.glt@gmail.com>
2024-05-10 23:13:13 +0530

    modified details banner and added videosSection

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import "./style.scss"
+import ContentWrapper from '../../components/contentWrapper/ContentWrapper'
 
 function PageNotFound() {
   return (
-    <div>PageNotFound</div>
+    <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
   )
 }
 
--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import "./style.scss"
-import DetailsBanner from './detailsBanner/DetailsBanner'
+import DetailsBanner from './detailsBanner/DetailsBanner';
+import Cast from "./cast/Cast";
+import VideosSection from "./videosSection/VideosSection";
+import { useParams } from 'react-router-dom';
+import useFetch from '../../hooks/useFetch';
 
 function Details() {
+
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+  const { data: credits, loading: creditsLoading } = useFetch(
+      `/${mediaType}/${id}/credits`
+  );
+
+
   return (
-    <div><DetailsBanner /></div>
+    <div>
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={creditsLoading} />
+            <VideosSection data={data} loading={loading} />
+      </div>
   )
 }
 
